edfd264922e7d47921a74cac60f7262ece8f1705
RhysWorkshare <rhys.davies@workshare.com>
2014-06-04 17:26:18 +0100

    Âupdate to reflect changes to agent in core, Agent reference now LocalAgent reference etc.

--- src/main/java/com/workshare/msnos/core_client/Bootstrap.java
+++ src/main/java/com/workshare/msnos/core_client/Bootstrap.java
@@ -1,27 +1,27 @@
 package com.workshare.msnos.core_client;
 
-import java.io.IOException;
-import java.util.UUID;
-
 import com.workshare.msnos.core.Cloud;
 import com.workshare.msnos.core.LocalAgent;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class Bootstrap {
 
     private static Cloud cloud;
-    private static LocalAgent agent;
+    private static LocalAgent microservice;
 
     public static Cloud grid() throws IOException {
         return cloud;
     }
 
     public static LocalAgent agent() throws IOException {
-        return agent;
+        return microservice;
     }
 
     public static void init() throws IOException {
         cloud = new Cloud(new UUID(111, 222));
-        agent = new LocalAgent(UUID.randomUUID());
+        microservice = new LocalAgent(UUID.randomUUID());
     }
 
 }
