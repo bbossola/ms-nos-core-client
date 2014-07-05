ba0515a33bd0a344f9d4e9b12670f743df777182
Bruno Bossola <bbossola@gmail.com>
2014-07-05 13:42:37 +0100

    Fixes to broken tests due to core Message refactoring

--- src/main/java/com/workshare/msnos/core_client/commands/PingCommand.java
+++ src/main/java/com/workshare/msnos/core_client/commands/PingCommand.java
@@ -2,6 +2,7 @@ package com.workshare.msnos.core_client.commands;
 
 import com.workshare.msnos.core.LocalAgent;
 import com.workshare.msnos.core.Message;
+import com.workshare.msnos.core.MessageBuilder;
 import com.workshare.msnos.core_client.Bootstrap;
 import com.workshare.msnos.core_client.Command;
 
@@ -15,6 +16,6 @@ public class PingCommand implements Command {
     @Override
     public void execute() throws Exception {
         LocalAgent me = Bootstrap.agent();
-        me.send(new Message(Message.Type.PIN, me.getIden(), Bootstrap.grid().getIden(), 2, false, null));
+        me.send(new MessageBuilder(Message.Type.PIN, me, Bootstrap.grid()).make());
     }
 }
